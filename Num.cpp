89f9783a0d5fd2ed40abd977125aeb935d6ca1ac
abbasizadeh <hossein.abbasizdeh@gmail.com>
2021-06-20 12:38:11 +0200

    simple example of using header file

---
+++ Num.cpp
@@ -0,0 +1,9 @@
+#include "Num.h"
+
+int Num (int );
+/*Num::Num() : num(0) { }
+Num::Num(int n): num(n) {}
+int Num::getNum()
+{
+ return num;
+}*/
---
+++ Num.h
@@ -0,0 +1,16 @@
+
+int Num (int a)
+{
+
+	return a*2;
+}
+
+/*class Num
+{
+ private:
+ int num;
+ public:
+ Num(int n);
+ int getNum();
+};*/ 
+
